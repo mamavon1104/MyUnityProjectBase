eea9a79191bd76ba3063a97d2a31d17003ee7750
popovon0325@gmail.com <popovon0325@gmail.com>
2024-10-29 04:32:55 +0900

    (大体一昨日と合わせて6+5時間くらい)長時間こいつとバトルして分かったことがある、そしてセーブしてしまったため Ctrl + Z しても戻らず、このプッシュをしたとしても俺の苦しみを理解できないのだ😿。
    keyboard,ps4コントローラーA(黒),ps4コントローラーB(赤)でやっていたましたが、両方「MODEL CUH-ZCT2J WIRELESS CONTROLLER DC 5V 800mA」の為識別が不可能である(nameを使おうが他の機能を8個ぐらい使おうがdeviceID以外は無理)
    そう、「deviceID」以外は...ね...😎
    
    と思ったらUnityのInputSystemから渡されるDeviceIDがなんか変で
    Disconnect 11
    Reconnect 12
    Disconnect 12
    Reconnect 13
    の様になんか再接続したのにDisconnectと同じ値が返ってこないとかいうちょっとあんまりな結果。🤔
    
    これだけ見ればContains(id -1)でいけるやん🥰と思うかもしれないけど
    
    (ps4コントローラ)A
    (ps4コントローラー)B
    でやると
    
    A Reconnected : 40。
    B Reconnected : 41。
    B Disconnected : 41。
    B Reconnected : 42。
    A Disconnected : 40。
    A ReConnected : 43。
    
    もう終わり、寝かせて、クソコでもいいでしょ、再接続と切断が同じIDだったら使えたわ、無理っす

--- Assets/SetPlayerInputAction.cs
+++ Assets/SetPlayerInputAction.cs
@@ -22,47 +22,19 @@ namespace Mamavon.Useful
 
         private void Awake()
         {
-            m_playerInputManager.joinAction += AddPlayerInputAction;
-            m_playerInputManager.leftAction += RemovePlayerInputAction;
             m_playerInputManager.deviceChangedAction += ReconnectDeviceAction;
         }
 
-        private void AddPlayerInputAction(PlayerInput playerInput)
-        {
-            Debug.Log("追加!");
-            if (playerInputDataList.Exists(data => data.playerInput == playerInput))
-                return;
-
-            PlayerInputData newData = new PlayerInputData
-            {
-                playerInput = playerInput,
-                devices = playerInput.devices
-            };
-            playerInputDataList.Add(newData);
-            playerInput.Debuglog("プレイヤーが参加", TextColor.Cyan);
-        }
-
-        private void RemovePlayerInputAction(PlayerInput playerInput)
-        {
-            int index = playerInputDataList.FindIndex(data => data.playerInput == playerInput);
-            if (index != -1)
-            {
-                playerInputDataList.RemoveAt(index);
-                playerInput.Debuglog("プレイヤーが退出", TextColor.Cyan);
-            }
-        }
 
         private void ReconnectDeviceAction(InputDevice device, InputDeviceChange deviceChange)
         {
-            if (deviceChange != InputDeviceChange.Reconnected)
-                return;
+            if (
+                deviceChange == InputDeviceChange.Reconnected ||
+                deviceChange == InputDeviceChange.Disconnected
+                )
 
-            var playerInputData = FindPlayerInputDataForDevice(device);
-            if (playerInputData != null)
-            {
-                playerInputData.playerInput.SwitchCurrentControlScheme(device);
-                Debug.Log($"リコネクト {device} to Player {playerInputData.playerInput}");
-            }
+                device.deviceId.Debuglog(deviceChange.ToString());
+
         }
 
         private PlayerInputData FindPlayerInputDataForDevice(InputDevice device)
